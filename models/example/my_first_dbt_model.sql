a61c87a531bdf48d3f92e52a76e9df976ad301e9
williamlinckdocket <124400095+williamlinckdocket@users.noreply.github.com>
2023-05-29 11:33:07 -0300

    Update my_first_dbt_model.sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -9,16 +9,10 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
 
-)
-
-select *
-from source_data
+select origem_atividade
+from crm.base_atividades
 
 /*
     Uncomment the line below to remove records with null `id` values

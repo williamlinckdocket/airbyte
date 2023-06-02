621071bd26e36bb27b0f682c9ef8e7b152d90aad
William Linck <william.linck@docket.com.br>
2023-06-02 14:25:17 +0000

    teste 3

--- models/example/cloud_dbt_test_tickets.sql
+++ models/example/cloud_dbt_test_tickets.sql
@@ -9,16 +9,10 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
 
 select *
-from source_data
+from homol.tickets
+limit 100
 
 /*
     Uncomment the line below to remove records with null `id` values
